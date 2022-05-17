cb340499d68a043c24064f614b9ae6985ff3ff26
vitorva <vitor.vazafonso@gmail.com>
2022-05-17 18:01:41 +0200

    wip

--- app/src/main/java/ch/and/pokemonpastropgo/MapsActivity.kt
+++ app/src/main/java/ch/and/pokemonpastropgo/MapsActivity.kt
@@ -1,7 +1,11 @@
 package ch.and.pokemonpastropgo
 
 import android.content.Intent
+import android.location.Location
+
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.Animation
@@ -9,12 +13,19 @@ import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ch.and.pokemonpastropgo.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+import com.google.android.gms.location.LocationRequest.create
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.*
+
+import com.google.android.gms.location.LocationRequest;
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
@@ -25,6 +36,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val fromBottomAnimation: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_animation) }
     private val toBottomAnimation: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_animation) }
 
+
+    var mapFrag: SupportMapFragment? = null
+    lateinit var mLocationRequest: LocationRequest
+    var mLastLocation: Location? = null
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    internal var mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                //The last location in the list is the newest
+                val location = locationList.last()
+                Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude())
+                mLastLocation = location
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker?.remove()
+                }
+
+                //Place current location marker
+                val latLng = LatLng(location.latitude, location.longitude)
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title("Current Position")
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+                mCurrLocationMarker = mMap.addMarker(markerOptions)
+
+                //move map camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11.0F))
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +91,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapsBinding.menuFab.setOnClickListener { animateFab() }
         mapsBinding.locationHintFab.setOnClickListener { Toast.makeText(this@MapsActivity, "Location hint", Toast.LENGTH_SHORT).show() }
         mapsBinding.historyBookFab.setOnClickListener { Toast.makeText(this@MapsActivity, "History book", Toast.LENGTH_SHORT).show() }
+
+        supportActionBar?.title = "Map Location Activity"
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        mapFrag = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFrag?.getMapAsync(this)
     }
+
+    public override fun onPause() {
+        super.onPause()
+
+        //stop location updates when Activity is no longer active
+        mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+    }
+
+
 
     // Handles return arrow button in MapsActivity ActionBar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -79,6 +138,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Moves the camera to show the entire area of interest
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(LausanneYverdonBounds, 200));
+
+        mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+
+        mLocationRequest =  LocationRequest().apply {
+            interval = 100
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            maxWaitTime = 100
+        }
+
+        mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+        mMap.isMyLocationEnabled = true
     }
 
     private var fabOpen = false
