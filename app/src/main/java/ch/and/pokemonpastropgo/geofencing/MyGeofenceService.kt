d630bc7a79992566700383cf8d7686bf1bbd4645
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2022-06-12 17:29:35 +0200

    Show history book

--- app/src/main/java/ch/and/pokemonpastropgo/geofencing/MyGeofenceService.kt
+++ app/src/main/java/ch/and/pokemonpastropgo/geofencing/MyGeofenceService.kt
@@ -176,11 +176,13 @@ class MyGeofenceService : Service() {
         return START_NOT_STICKY
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
+    override fun onBind(intent: Intent?): IBinder {
         return mBinder
     }
 
     override fun onDestroy() {
+        removeGlobalGeofenceRequest()
+        removeUniqueGeofenceRequest()
     }
 
 
